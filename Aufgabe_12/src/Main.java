a48cb9e1333928c08c8998d2a2b2feedace95059
Sarah <sarahchristen74@gmail.com>
2024-10-02 11:39:22 +0200

    Aufgabe 12

---
+++ Aufgabe_12/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        do {
+            doSomething(); // kann i manipulieren
+        } while (i >= 0);
+
+        doSomething();
+        while (i >= 0) {
+            doSomething();
+        }
+    }
+}
