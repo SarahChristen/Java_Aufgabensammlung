16ee903ac8320c4f54819b9f11ce345cfa8f03ff
Sarah <sarahchristen74@gmail.com>
2024-10-04 10:47:28 +0200

    Aufgabe 19 try-catch

--- 19_Rechner/src/Main.java
+++ 19_Rechner/src/Main.java
@@ -1,3 +1,4 @@
+import java.time.format.DateTimeParseException;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -9,50 +10,56 @@ public class Main {
         int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         while (!quit) {
-            boolean foundSymbol = false;
-            char mathsymbol = '0';
+            try {
+                boolean foundSymbol = false;
+                char mathsymbol = '0';
 
-            System.out.println("Make your calculation (or press Q to quit): ");
-            String input = s.nextLine();
-            if (Objects.equals(input, "q")) {
-                quit = true;
-            } else {
-                input = input.replaceAll(" ", "");
-                int num1 = 0;
-                int num2 = 0;
+                System.out.println("Make your calculation (or press Q to quit): ");
+                String input = s.nextLine();
+                if (Objects.equals(input, "q")) {
+                    quit = true;
+                } else {
+                    input = input.replaceAll(" ", "");
+                    int num1 = 0;
+                    int num2 = 0;
 
-                for (int i = 0; i < input.length(); i++) {
-                    char symbol = input.charAt(i);
-                    for (int j = 0; j < numbers.length; j++) {
-                        int num = symbol - '0';
-                        if (num == numbers[j]) {
-                            if (!foundSymbol) {
-                                num1 = num1 * 10 + num;
-                            } else {
-                                num2 = num2 * 10 + num;
+                    for (int i = 0; i < input.length(); i++) {
+                        char symbol = input.charAt(i);
+                        for (int j = 0; j < numbers.length; j++) {
+                            int num = symbol - '0';
+                            if (num == numbers[j]) {
+                                if (!foundSymbol) {
+                                    num1 = num1 * 10 + num;
+                                } else {
+                                    num2 = num2 * 10 + num;
+                                }
                             }
                         }
+                        if (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/') {
+                            mathsymbol = symbol;
+                            foundSymbol = true;
+                        }
                     }
-                    if (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/') {
-                        mathsymbol = symbol;
-                        foundSymbol = true;
+                    switch (mathsymbol) {
+                        case '+':
+                            result = num1 + num2;
+                            break;
+                        case '-':
+                            result = num1 - num2;
+                            break;
+                        case '*':
+                            result = num1 * num2;
+                            break;
+                        case '/':
+                            result = num1 / num2;
+                            break;
                     }
+                    System.out.println(result);
                 }
-                switch (mathsymbol) {
-                    case '+':
-                        result = num1 + num2;
-                        break;
-                    case '-':
-                        result = num1 - num2;
-                        break;
-                    case '*':
-                        result = num1 * num2;
-                        break;
-                    case '/':
-                        result = num1 / num2;
-                        break;
-                }
-                System.out.println(result);
+            } catch (ArithmeticException ex) {
+                System.out.println("Durch 0 teilen ist nicht erlaubt");
+            } catch (Exception ex) {
+                System.out.println("Unbekannter Fehler: " + ex);
             }
         }
     }
